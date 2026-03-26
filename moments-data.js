e2f9d9a60e8d1830a276f8995cad4b8679460be9
nakataas <nakataas@github.com>
2026-03-26 22:08:09 +0700

    Add new personal moments to the timeline

--- moments-data.js
+++ moments-data.js
@@ -1,12 +1,20 @@
 const momentsData = [
     {
+        id: 15,
+        date: '2026-03-25',
+        title: 'PVJ Date & Pepper Lunch',
+        description: 'Jalan-jalan ke PVJ habis LDR seminggu makan di Pepper Lunch dan beli body lotion and cat rambut. 🛋️🍽️🧴💇🏻‍♀️',
+        photos: 'https://photos.app.goo.gl/AeDsHPuvaJ9Wr9mC6',
+        type: 'past',
+        isLatest: true
+    },
+    {
         id: 7,
         date: '2026-03-17',
         title: 'Kiara Artha Park & Quick Dentist Trip',
         description: 'Makan masakkan doi (Udang saus padang dan ayam suwir daun jeruk) di mobil di Kiara Artha Park. Lalu tiba-tiba ke dokter gigi karena gigi Silvia sakit, di sana periksa lanjut pulangnya ke Richeese makan lagi. 🍤🍗🦷🍗',
         photos: 'https://photos.app.goo.gl/HhVnL5kCNFJpkT2M8',
-        type: 'past',
-        isLatest: true
+        type: 'past'
     },
     {
         id: 8,
@@ -62,6 +70,94 @@ const momentsData = [
         title: 'Cooking Date & AEON Shopping',
         description: 'Belanja bahan masak bareng di AEON PVJ. Seru banget akhirnya kita masak sendiri di apart sambil nunggu waktu buka puasa. Dinner hasil masakan sendiri emang beda rasanya! 🛒🥘🌙',
         photos: 'https://photos.app.goo.gl/nfQmXkk7KCkm8hDT9',
+        type: 'past'
+    },
+    {
+        id: 16,
+        date: '2026-02-25',
+        title: 'PVJ & Kue Balok',
+        description: 'Buka bersama di PVJ lalu beli kue balok. 🍢🧁🌙',
+        photos: 'https://photos.app.goo.gl/TcZTdxFV5GEmgjjB7',
+        type: 'past'
+    },
+    {
+        id: 17,
+        date: '2026-02-23',
+        title: 'Dentist Visit & Saparua Snacks',
+        description: 'Silvia nemenin ke Dokter Gigi lalu udahnya jajan di Saparua. 🦷🥨🥤',
+        photos: 'https://photos.app.goo.gl/z79svMu93t14vcAC7',
+        type: 'past'
+    },
+    {
+        id: 18,
+        date: '2026-02-22',
+        title: 'Bukber with Silvia\'s Sister & Seblak',
+        description: 'Buka bersama bareng kakaknya Silvia, lalu dilanjut makan seblak bareng. 🍲🔥🌙',
+        photos: 'https://photos.app.goo.gl/f2h5JFzMG7J7Kcpn9',
+        type: 'past'
+    },
+    {
+        id: 19,
+        date: '2026-02-16',
+        title: 'Rumah Guguk & Braga Photobooth',
+        description: 'Main ke Rumah Guguk buat liat anjing-anjing lucu, lalu lanjut ke Braga buat photobooth berdua. 🐶📸✨',
+        photos: 'https://photos.app.goo.gl/fMmJHb8xs1izvrxPA',
+        type: 'past'
+    },
+    {
+        id: 20,
+        date: '2026-02-14',
+        title: 'Valentine Date Dinner',
+        description: 'Makan malam romantis untuk ngerayain Valentine di Mercure Bandung. 💖🥂🍷',
+        photos: 'https://photos.app.goo.gl/81UiqDZwX9QjbzJv8',
+        type: 'past'
+    },
+    {
+        id: 21,
+        date: '2026-02-12',
+        title: 'Pancake Date & Ciwalk Stroll',
+        description: 'Makan pancake yang manis, lalu lanjut jalan-jalan santai di Ciwalk. 🥞🚶🏻‍♂️🚶🏻‍♀️',
+        photos: 'https://photos.app.goo.gl/eCUPJHHiYUounarn6',
+        type: 'past'
+    },
+    {
+        id: 22,
+        date: '2026-02-10',
+        title: 'Summarecon Steak Dinner',
+        description: 'Makan steak enak di Summarecon habis nemenin Silvia ke tukang jahit. 🥩🧵🍽️',
+        photos: 'https://photos.app.goo.gl/vDv42RecMBBxt2X59',
+        type: 'past'
+    },
+    {
+        id: 23,
+        date: '2026-02-08',
+        title: 'Lembang Zoo Date & Home-cooked Meal',
+        description: 'Serunya nge-date di Lembang Park and Zoo, apalagi sambil makan bekel masakan spesial dari Silvia. 🦁🍱❤️',
+        photos: 'https://photos.app.goo.gl/DKDrDThWbukcZzUk7',
+        type: 'past'
+    },
+    {
+        id: 24,
+        date: '2026-02-06',
+        title: 'Gasibu Sate Asin Date',
+        description: 'Jajan sate asin di Gasibu, ditemenin Silvia yang baru beres dari tukang jahit. 🍢🏙️🌙',
+        photos: 'https://photos.app.goo.gl/M2XLPLjpqYDyBh4z8',
+        type: 'past'
+    },
+    {
+        id: 25,
+        date: '2026-02-03',
+        title: 'Cat Cafe & Sushi Date',
+        description: 'Main sama kucing-kucing lucu di Cat Cafe PVJ, lalu tutup hari dengan makan sushi habis nemenin Silvia belanja. 🐱🍣🛒',
+        photos: 'https://photos.app.goo.gl/eEQHdeCie1Kje689A',
+        type: 'past'
+    },
+    {
+        id: 26,
+        date: '2026-02-01',
+        title: 'Garut Trip!',
+        description: 'Perjalanan seru nemenin Silvia ke Garut, dan pastinya wisata kuliner makan enak di sana. ⛰️🍲🚗',
+        photos: 'https://photos.app.goo.gl/FKu2x6iJrkYZJ6MZA',
         type: 'past'
     },
     {
